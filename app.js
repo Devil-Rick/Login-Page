f840c02967cae989de67e48c90df46cbd755fc2d
Devil-Rick <saptarshinaruto@gmail.com>
2020-12-18 17:30:10 +0530

    md5style

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+console.log(md5('message'));
 
 
 
@@ -23,8 +24,6 @@ const userSchema =new mongoose.Schema( {
   password:String
 })
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt,{secret:secret , encryptedFields:['password']});//for multiple fields add a comma
 
 const User = new mongoose.model("User",userSchema);
 // using route method for easier understanding
@@ -42,7 +41,7 @@ app.route("/register")
 .post(function(req,res){
   const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   })
 
   newUser.save(function(err){
@@ -62,7 +61,7 @@ app.route("/login")
 })
 .post(function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:username} , function(err,foundUser){
     if (err) {
