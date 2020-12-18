c100a8ae2b0ad63eb4fa37fd4c0376be2b3e7c4b
Devil-Rick <saptarshinaruto@gmail.com>
2020-12-18 18:26:26 +0530

    using bcrypt

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require('md5');
-console.log(md5('message'));
-
+const bcrypt = require('bcrypt');
+//salting means adding a random number to the password provided by the user and the creating the hash function.
+// This helps in enhancin security for easier and dictionary passwords.
+// salt round means the no. of times the same passwords r applied to increase the security
+const saltRounds = 10; //show the number of salt rounds
 
 
 
@@ -39,18 +41,20 @@ app.route("/register")
   res.render("register");
 })
 .post(function(req,res){
-  const newUser = new User({
-    email:req.body.username,
-    password:md5(req.body.password)
-  })
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+    })
 
-  newUser.save(function(err){
-    if (!err) {
-      res.render("secrets")
-    } else {
-      console.log(err);
-    }
-  })
+    newUser.save(function(err){
+      if (!err) {
+        res.render("secrets")
+      } else {
+        console.log(err);
+      }
+    })
+  });
 });
 
 
@@ -61,16 +65,18 @@ app.route("/login")
 })
 .post(function(req,res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email:username} , function(err,foundUser){
     if (err) {
       console.log(err);
     } else {
       if(foundUser){
-        if (foundUser.password === password) {
-          res.render("secrets")
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result===true) {
+            res.render("secrets")
+          }
+        });
       }
     }
   })
