d9ee657d7010c89be93a48ac1ef603abb469bf59
Devil-Rick <saptarshinaruto@gmail.com>
2020-12-22 14:49:25 +0530

    using passport.js

--- app.js
+++ app.js
@@ -4,32 +4,56 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-//salting means adding a random number to the password provided by the user and the creating the hash function.
-// This helps in enhancin security for easier and dictionary passwords.
-// salt round means the no. of times the same passwords r applied to increase the security
-const saltRounds = 10; //show the number of salt rounds
+// additional files for using passport method
+const session = require('express-session')
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 
 
 const app = express();
-
+// intializing ejs bodyParser and express
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//  setting up and intializing express-session
+app.use(session({
+  secret: 'this is my secret', //this part will be moved to env file
+  resave: false,
+  saveUninitialized: true
+}))
+
+
+// intializing passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDb", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex" , true); //depcrecation // WARNING:
+
 
 const userSchema =new mongoose.Schema( {
   email:String,
   password:String
 })
 
+// passportLocalMongoose is a plugin like the encrytion method
+userSchema.plugin(passportLocalMongoose);
+
+
 
 const User = new mongoose.model("User",userSchema);
-// using route method for easier understanding
 
+// must be under User
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// using route method for easier understanding
 // working of the home pagee getting information
 app.route("/").get(function(req,res){
   res.render("home");
@@ -41,20 +65,16 @@ app.route("/register")
   res.render("register");
 })
 .post(function(req,res){
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-      email:req.body.username,
-      password:hash
-    })
-
-    newUser.save(function(err){
-      if (!err) {
-        res.render("secrets")
-      } else {
-        console.log(err);
-      }
-    })
-  });
+  User.register({username:req.body.username}, req.body.password , function(err,user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register")
+    } else {
+      passport.authenticate("local")(req , res , function(){
+        res.redirect("/secrets")
+      })
+    }
+  })
 });
 
 
@@ -64,23 +84,46 @@ app.route("/login")
   res.render("login");
 })
 .post(function(req,res){
-  const username = req.body.username;
-  const password = req.body.password;
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
-  User.findOne({email:username} , function(err,foundUser){
+  req.login(user , function(err){
     if (err) {
       console.log(err);
     } else {
-      if(foundUser){
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          if (result===true) {
-            res.render("secrets")
-          }
-        });
-      }
+      passport.authenticate("local",{
+        // when the password is wrong
+        failureRedirect: '/login'
+      })(req,res,function(){
+        res.redirect("/secrets")
+      });
     }
   })
 });
+
+
+
+// working of the secrets page not required for md5 encrytion or bycrypt method
+// as the user is already authenticated in this case this route is being created
+app.route("/secrets")
+.get(function(req,res){
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+})
+
+
+// last and final the logout page
+app.route("/logout")
+.get(function(req,res){
+  req.logout();
+  res.redirect("/")
+})
+
 
 
 
